b1a59f1b8822fdbdad43736105be18c5dbcff48d
Ug0510 <uditgg0510@gmail.com>
2023-07-27 17:57:05 +0530

    maskTaskAsComplete function added

--- Mini Projects/Todo-list/todo.js
+++ Mini Projects/Todo-list/todo.js
@@ -1,4 +1,4 @@
-const tasks = [];
+let tasks = [];
 const taskList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
@@ -7,25 +7,41 @@ console.log('Working');
 
 function renderList () {
 
-    for (task of tasks){
+    // for (task of tasks){
         
-        const listItem = ```<li>
-        <input type="checkbox" id="task{{$task.id}}" data-id="12" class="custom-checkbox">
-        <label for="task1">Buy groceries</label>
-        <img src="bin.svg" class="delete" data-id="12" />
-      </li>```;
-    }
+    //     const listItem = ```<li>
+    //     <input type="checkbox" id="task{{$task.id}}" data-id="12" class="custom-checkbox">
+    //     <label for="task1">Buy groceries</label>
+    //     <img src="bin.svg" class="delete" data-id="12" />
+    //   </li>```;
+    // }
 
+    console.log(tasks);
 }
 
-function markTaskAsComplete (taskId) {}
+function markTaskAsComplete (taskId) {
+    const task = tasks.filter(function(task){
+        return task.id === taskId;
+    })
+
+    if(task.length > 0)
+    {
+        const currentTask = task[0];
+        currentTask.done = !currentTask.done;
+        renderList();
+        showNotification('Task Toggle Successfull')
+    }
+    showNotification('Unable to toggle the task')
+}
 
 function deleteTask (taskId) {
 
     const newTasks = tasks.filter(function(task){
-        if (task.id != taskId)
-            return task;
+        return task.id != taskId;
     })
+    tasks = newTasks;
+    renderList();
+    showNotification('Task deleted successfull');
 }
 
 function addTask (task) {
@@ -35,7 +51,6 @@ function addTask (task) {
         //adding task in the list;
     tasks.push(task);
 
-    console.log(tasks);
     //rendering list to update the list of todo itms
     renderList();
     showNotification('Task Added Successfully!');
@@ -65,7 +80,7 @@ function handleInputKeypress(e){
         const task = {
             text,
             id: Date.now().toString(),
-            dont: 'false'
+            done: 'false'
         };
         e.target.value = "";
         addTask(task);
