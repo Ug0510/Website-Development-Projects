7c65a00948eff2b758fef0ff053f16da5c7f3560
Ug0510 <uditgg0510@gmail.com>
2023-07-28 08:29:20 +0530

    Todos called from an API

--- Mini Projects/Todo-list/todo.js
+++ Mini Projects/Todo-list/todo.js
@@ -3,14 +3,29 @@ const taskList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
 
+function fetchTodos(){
+
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        tasks = data.slice(0,10);
+        renderList();
+    })
+    .catch(error => console.log(error))
+}
+
+
+
 
 function addTaskToDOM(task){
 
     const li = document.createElement('li');
 
     li.innerHTML = `
-          <input type="checkbox" id="${task.id}" ${task.done? 'checked' : ''} class="custom-checkbox">
-          <label for="${task.id}">${task.text}</label>
+          <input type="checkbox" id="${task.id}" ${task.completed? 'checked' : ''} class="custom-checkbox">
+          <label for="${task.id}">${task.title}</label>
           <img src="./icons/bin.png" class="delete" data-id="${task.id}" />
     `;
     taskList.append(li);
@@ -31,13 +46,13 @@ function renderList () {
 
 function markTaskAsComplete (taskId) {
     const task = tasks.filter(function(task){
-        return task.id === taskId;
+        return Number(task.id) === Number(taskId);
     })
 
     if(task.length > 0)
     {
         const currentTask = task[0];
-        currentTask.done = !currentTask.done;
+        currentTask.completed = !currentTask.completed;
         renderList();
         showNotification('Task Toggle Successfull')
         return;
@@ -72,25 +87,25 @@ function addTask (task) {
 
 }
 
-function showNotification(text) {
-    alert(text);
+function showNotification(title) {
+    alert(title);
 }
 
 function handleInputKeypress(e){
     if (e.key == 'Enter'){
-        const text = e.target.value;
+        const title = e.target.value;
 
         //if input field is empty when enter key is pressed
-        if(!text)
+        if(!title)
         {
-            showNotification('Task Text can\'t be empty');
+            showNotification('Task title can\'t be empty');
             return;
         }
 
         const task = {
-            text,
-            id: Date.now().toString(),
-            done: false
+            title,
+            id: Date.now(),
+            completed: false
         };
         e.target.value = "";
         addTask(task);
@@ -115,7 +130,7 @@ function handleClickListener(event){
 }
 
 function initializeApp(){
-
+fetchTodos();
 addTaskInput.addEventListener('keyup',handleInputKeypress);
 
 taskList.addEventListener('click',handleClickListener);
